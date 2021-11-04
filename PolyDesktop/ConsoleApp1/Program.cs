8ab3082c5706828f4dc387e9a87f3110b69ec2a3
reeblucas <40370208+reeblucas@users.noreply.github.com>
2021-11-04 12:30:10 -0700

    Initial Commit
    Created 2 Projects
      PolyDesktopLauncher - C# .Net Core 5.0
      PolyDesktopGUI - C#/UWP
    Added Tray Icon and Banner in Assets folder

---
+++ PolyDesktop/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+/**************************************************************
+ * Copyright (c) 2021
+ * Author: Tyler Lucas
+ * Filename: Program.cs
+ * Date Created: 11/4/2021
+ * Modifications:
+ **************************************************************/
+/**************************************************************
+ * Overview:
+ *      
+ **************************************************************/
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
