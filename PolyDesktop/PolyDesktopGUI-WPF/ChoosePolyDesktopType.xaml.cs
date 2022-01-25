05423e546bb473bef5e88f5080ac8b1bf70096b6
tyler-lucas <40370208+tyler-lucas@users.noreply.github.com>
2022-01-25 11:31:13 -0800

    Completed Start PolyDesktop Button
    Created StartPDButton and added functionality
    Created ChoosePolyDesktopType popup, front-end only

--- PolyDesktop/PolyDesktopGUI-WPF/ChoosePolyDesktopType.xaml.cs
+++ PolyDesktop/PolyDesktopGUI-WPF/ChoosePolyDesktopType.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PolyDesktopGUI_WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ChoosePolyDesktopType.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ChoosePolyDesktopType : Window
     {
-        public MainWindow()
+        public ChoosePolyDesktopType()
         {
             InitializeComponent();
-        }
-
-        private void StartPD_Click(object sender, RoutedEventArgs e)
-        {
-            var CPD = new ChoosePolyDesktopType();
-            CPD.Show();
         }
     }
 }
