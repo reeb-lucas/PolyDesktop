a05e66f0edc6514d963132609e9b5af4522032d9
Nate Arnold <nate.arnold@oit.edu>
2022-02-27 21:29:29 -0800

    Initial Commit of Chat Application
    Includes a ChatClient that runs the chat application client(Still very basic and needs work). And a Server console application that starts a server and gives some information on the connections and messages sent by clients.

---
+++ PolyDesktop/ServerClientChatApp/Net/Server.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using ChatClient.Net.IO;
+using ChatServer.Net.IO;
+
+namespace ChatClient.Net
+{
+    class Server
+    {
+        TcpClient _client;
+        public PacketReader PacketReader;
+
+        public event Action connectedEvent;
+        public event Action msgReceivedEvent;
+        public event Action userDisconnectedEvent;
+
+        public Server()
+        {
+            _client = new TcpClient();
+        }
+
+        public void ConnectToSever(string username)
+        {
+            if(!_client.Connected)
+            {
+                _client.Connect("10.32.61.39", 5906);
+                PacketReader = new PacketReader(_client.GetStream());
+
+                if (!string.IsNullOrEmpty(username))
+                {
+                    var connectPacket = new PacketBuilder();
+                    connectPacket.WriteOpCode(0);
+                    connectPacket.WriteMessage(username);
+                    _client.Client.Send(connectPacket.GetPacketBytes());
+                }
+
+                ReadPackets();
+            }
+        }
+
+        private void ReadPackets()
+        {
+            Task.Run(() =>
+            { 
+                while(true)
+                {
+                    var opcode = PacketReader.ReadByte();
+                    switch(opcode)
+                    {
+                        case 1:
+                            connectedEvent?.Invoke();
+                            break;
+                        case 5:
+                            msgReceivedEvent?.Invoke();
+                            break;
+                        case 10:
+                            userDisconnectedEvent?.Invoke();
+                            break;
+                        default:
+                            Console.WriteLine("???");
+                            break;
+                    }
+                }
+            });
+        }
+
+        public void SendMessageToServer(string message)
+        {
+            var messagePacket = new PacketBuilder();
+            messagePacket.WriteOpCode(5);
+            messagePacket.WriteMessage(message);
+            _client.Client.Send(messagePacket.GetPacketBytes());
+        }
+    }
+}
