ba0ffe727776112a41d0b805ba50bbd6b5589a49
Jacob Pressley <57932476+japressley8@users.noreply.github.com>
2022-03-30 22:43:12 -0700

    Created an advanced connection option and created the settings page

--- PolyDesktop/PolyDesktopGUI-WPF/MainWindow.xaml.cs
+++ PolyDesktop/PolyDesktopGUI-WPF/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace PolyDesktopGUI_WPF
             InitializeComponent();
             ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncWithAppMode;
             ThemeManager.Current.SyncTheme();
+            App.Current.Properties["AdvancedMode"] = false;
         }
         private void EditDeskPre_Click(object sender, RoutedEventArgs e)
         {
@@ -59,32 +60,42 @@ namespace PolyDesktopGUI_WPF
         {
             ModePickerFlyout.IsOpen = false;
         }
-
         private void Settings_Click(object sender, RoutedEventArgs e)
         {
             ModePickerFlyout.IsOpen = false;
+            NavFrame.Navigate(new SettingsPage()); //Settings
         }
         private void StartPDButton_Click(object sender, RoutedEventArgs e)
         {
+            if((bool)(App.Current.Properties["AdvancedMode"]) == true)
+            {
+                AdvancedButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                AdvancedButton.Visibility = Visibility.Hidden;
+            }
             ModePickerFlyout.IsOpen = true;
         }
-
         private void BasicButton_Click(object sender, RoutedEventArgs e)
         {
             ModePickerFlyout.IsOpen = false;
             NavFrame.Navigate(new BasicModePage()); //Basic
         }
-
         private void TabButton_Click(object sender, RoutedEventArgs e)
         {
             ModePickerFlyout.IsOpen = false;
             NavFrame.Navigate(new TabModePage()); //Tabs
         }
-
         private void GroupButton_Click(object sender, RoutedEventArgs e)
         {
             ModePickerFlyout.IsOpen = false;
             NavFrame.Navigate(new GroupModePage()); //Group
+        }
+        private void AdvancedButton_Click(object sender, RoutedEventArgs e)
+        {
+            ModePickerFlyout.IsOpen = false;
+            NavFrame.Navigate(new AdvancedModePage()); //Advanced
         }
     }
 }
