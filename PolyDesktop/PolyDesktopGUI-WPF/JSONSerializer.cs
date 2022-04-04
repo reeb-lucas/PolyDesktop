0392b95a253fae1b1df838a8660755cb2f0634d2
Jerron-Rhen <84877202+Jerron-Rhen@users.noreply.github.com>
2022-04-03 20:27:35 -0700

    Standard polydesktop --> student version
    Completely refacted and added the starting wireframe for the student side polybay and polychat

---
+++ PolyDesktop/PolyDesktopGUI-WPF/JSONSerializer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace NetworkCommsDotNet.DPSBase
+{
+    [DataSerializerProcessor(4)]
+    public class JSONSerializer : DataSerializer
+    {
+
+        private JSONSerializer() { }
+
+        #region ISerialize Members
+
+        /// <inheritdoc />
+        protected override void SerialiseDataObjectInt(Stream outputStream, object objectToSerialise, Dictionary<string, string> options)
+        {
+            if (outputStream == null)
+                throw new ArgumentNullException("outputStream");
+
+            if (objectToSerialise == null)
+                throw new ArgumentNullException("objectToSerialize");
+
+            outputStream.Seek(0, 0);
+            var data = Encoding.Unicode.GetBytes(JsonConvert.SerializeObject(objectToSerialise));
+            outputStream.Write(data, 0, data.Length);
+            outputStream.Seek(0, 0);
+        }
+
+        /// <inheritdoc />
+        protected override object DeserialiseDataObjectInt(Stream inputStream, Type resultType, Dictionary<string, string> options)
+        {
+            var data = new byte[inputStream.Length];
+            inputStream.Read(data, 0, data.Length);
+            return JsonConvert.DeserializeObject(new String(Encoding.Unicode.GetChars(data)), resultType);
+        }
+
+        #endregion
+    }
+}
--- PolyDesktop/PolyDesktopGUI-WPF/MainWindow.xaml.cs
+++ PolyDesktop/PolyDesktopGUI-WPF/MainWindow.xaml.cs
@@ -15,8 +15,11 @@
 using ControlzEx.Theming;
 using MahApps.Metro.Controls;
 using System;
+using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Navigation;
+using NetworkCommsDotNet.Connections;
 
 namespace PolyDesktopGUI_WPF
 {
@@ -25,64 +28,60 @@ namespace PolyDesktopGUI_WPF
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private List<MetroTabItem> m_tabItemList;
         public MainWindow()
         {
             InitializeComponent();
-            ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncWithAppMode;
-            ThemeManager.Current.SyncTheme();
-            App.Current.Properties["AdvancedMode"] = false;
-        }
-        private void EditDeskPre_Click(object sender, RoutedEventArgs e)
-        {
-            ModePickerFlyout.IsOpen = false;
-            NavFrame.Navigate(new EditPresets());
+
+            m_tabItemList = new List<MetroTabItem>();
+            //  m_VNCList = new List<VncPage>();
+
+            //this tab will hold PolyChat
+            MetroTabItem tabPolyChat = new MetroTabItem();
+            tabPolyChat.Header = "PolyChat";
+
+            Frame PolyCFrame = new Frame();
+            tabPolyChat.Content = PolyCFrame;
+            PolyChat polyChat = new PolyChat();
+            PolyCFrame.Navigate(polyChat);
+            m_tabItemList.Add(tabPolyChat);
+
+
+            //this tab will hold PolyBay
+            MetroTabItem tabPolyBay = new MetroTabItem();
+            tabPolyBay.Header = "PolyBay";
+
+            Frame PolyBFrame = new Frame();
+            tabPolyBay.Content = PolyBFrame;
+            PolyBay polyBay = new PolyBay();
+            PolyBFrame.Navigate(polyBay);
+            m_tabItemList.Add(tabPolyBay);
+
+            
+
+            //bind tab list
+            tabControl.DataContext = m_tabItemList;
+            tabControl.SelectedIndex = 0;
+
         }
 
-        private void ViewDeskPro_Click(object sender, RoutedEventArgs e)
+        private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            ModePickerFlyout.IsOpen = false;
-            NavFrame.Navigate(new DesktopProperties());
-        }
-        /// <summary>
-        /// Uses navString to change the frame to the .xaml specified
-        /// </summary>
-        /// <param name="navString">
-        /// Contains a .xaml file name
-        /// </param>
-        public void Nav(string navString)
-        {
-            Uri uri = new Uri(navString, UriKind.Relative);
+            MetroTabItem tab = tabControl.SelectedItem as MetroTabItem;
+            if (tab != null && tab.Header != null)
+            {
+                //if (tab.Header.Equals("+"))
+                //{
+                //    //clear tabControl binding
+                //    tabControl.DataContext = null;
 
-            NavFrame.Navigate(uri);
+                //    //adds new tab, binds tab control, and seclects
+                //    MetroTabItem newTab = this.AddTabItem();
+                //    tabControl.DataContext = m_tabItemList;
+                //    tabControl.SelectedItem = newTab;
+                //}
+            }
         }
-
-        private void Tutorial_Click(object sender, RoutedEventArgs e)
-        {
-            ModePickerFlyout.IsOpen = false;
-        }
-        private void Settings_Click(object sender, RoutedEventArgs e)
-        {
-            ModePickerFlyout.IsOpen = false;
-            NavFrame.Navigate(new SettingsPage()); //Settings
-        }
-        private void StartPDButton_Click(object sender, RoutedEventArgs e)
-        {
-            ModePickerFlyout.IsOpen = true;
-        }
-        private void BasicButton_Click(object sender, RoutedEventArgs e)
-        {
-            ModePickerFlyout.IsOpen = false;
-            NavFrame.Navigate(new BasicModePage()); //Basic
-        }
-        private void TabButton_Click(object sender, RoutedEventArgs e)
-        {
-            ModePickerFlyout.IsOpen = false;
-            NavFrame.Navigate(new TabModePage()); //Tabs
-        }
-        private void GroupButton_Click(object sender, RoutedEventArgs e)
-        {
-            ModePickerFlyout.IsOpen = false;
-            NavFrame.Navigate(new GroupModePage()); //Group
-        }
+       
     }
 }
