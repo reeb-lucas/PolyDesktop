5e6889e4c7985a98982c85048703f5859ff63225
Nate Arnold <nate.arnold@oit.edu>
2022-05-18 01:19:28 -0700

    Fixed bugs from merge
    Fixed the error messages having the wrong formatting.

--- PolyDesktop/PolyDesktopGUI-WPF/Net/Server.cs
+++ PolyDesktop/PolyDesktopGUI-WPF/Net/Server.cs
@@ -20,7 +20,6 @@ namespace ChatClient.Net
         public event Action UserDisconnectedEvent;
         public event Action PopHelpQueueEvent;
         public PacketBuilder errorPacket = new PacketBuilder();
-        private byte errCode = 2;
 
         public Server()
         {
@@ -39,8 +38,8 @@ namespace ChatClient.Net
                     if (!string.IsNullOrEmpty(username))
                     {
                         var connectPacket = new PacketBuilder();
-                        //connectPacket.WriteOpCode(0);
-                        connectPacket.WriteMessage(0,username);
+                        connectPacket.WriteOpCode(0);
+                        connectPacket.WriteMessage(username);
                         _client.Client.Send(connectPacket.GetPacketBytes());
                     }
 
@@ -48,7 +47,7 @@ namespace ChatClient.Net
                 }
                 catch
                 {
-                    errorPacket.WriteMessage(errCode,"Error 1: Connection to Chat Server failed.");
+                    errorPacket.WriteMessage("Error 1: Connection to Chat Server failed.");
                 }
             }
         }
@@ -60,21 +59,21 @@ namespace ChatClient.Net
                 if (_client.Connected)
                 {
                     var requestPacket = new PacketBuilder();
-                    //requestPacket.WriteOpCode(15);
-                    requestPacket.WriteMessage(15,username);
+                    requestPacket.WriteOpCode(15);
+                    requestPacket.WriteMessage(username);
                     try
                     {
                         _client.Client.Send(requestPacket.GetPacketBytes());
                     }
                     catch
                     {
-                        errorPacket.WriteMessage(errCode,"Error 2: help request failed.");
+                        errorPacket.WriteMessage("Error 2: help request failed.");
                     }
                 }
             }
             catch
             {
-                errorPacket.WriteMessage(errCode,"Error 1: Connection to Chat Server failed.");
+                errorPacket.WriteMessage("Error 1: Connection to Chat Server failed.");
             }
         }
 
@@ -111,7 +110,7 @@ namespace ChatClient.Net
                     }
                     catch 
                     {
-                        errorPacket.WriteMessage(errCode,"Error 3: Forcefully disconnected from host.");
+                        errorPacket.WriteMessage("Error 3: Forcefully disconnected from host.");
                     }
                 }
             });
@@ -120,15 +119,15 @@ namespace ChatClient.Net
         public void SendMessageToServer(string message)
         {
             var messagePacket = new PacketBuilder();
-           // messagePacket.WriteOpCode(5);
-            messagePacket.WriteMessage(5,message);
+            messagePacket.WriteOpCode(5);
+            messagePacket.WriteMessage(message);
             try
             {
                 _client.Client.Send(messagePacket.GetPacketBytes());
             }
             catch
             {
-                errorPacket.WriteMessage(errCode,"Error 4: failed to send message.");
+                errorPacket.WriteMessage("Error 4: failed to send message.");
             }
         }
     }
